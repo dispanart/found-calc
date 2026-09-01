9d3f5835b138cac1b195bcfeabff12d57d678dda
dispanart <86879580+dispanart@users.noreply.github.com>
2026-09-01 12:11:38 +0700

    feat(engine): export Phase 08A calculator truth

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -3,3 +3,6 @@ export * from "./decimal";
 export * from "./reference/discount";
 export * from "./reference/business-margin";
 export * from "./reference/synthetic-rule";
+export * from "./quick/percentage";
+export * from "./quick/date-difference";
+export * from "./quick/length-conversion";
